b5a2fa772b829ad74ff30bc5db9745f98348e789
Jan Amann <jan@amann.me>
2020-07-02 17:51:11 +0200

    fix transpile modules

--- packages/example/next.config.js
+++ packages/example/next.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const withImages = require('next-images');
-const withTM = require('next-transpile-modules')(['next-client-script']);
 const withClientScripts = require('next-client-script/dist/withClientScripts');
 
 const nextConfig = {
